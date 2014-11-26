1d8db1f1c112c02fc87de3aad7209784b7a17fb7
Pierre Gay <pierre.gay@u-bordeaux.fr>
2014-11-26 16:54:06 +0100

    move irods generic code to specific module

---
+++ replcheck/icommand.py
@@ -0,0 +1,79 @@
+# -*- python -*-
+
+import os
+import re
+import subprocess
+
+class IrodsCommand:
+    "An iRODS iCommand wrapper"
+    def __init__(self, cmd, opts = [], output_filter = lambda e: e, verbose = False):
+        self.cmd = cmd
+        self.opts = opts
+        self.verbose = verbose
+        self.output_filter = output_filter
+
+    def __call__(self, cmdline = []):
+        cmdlist = [self.cmd] + self.opts + cmdline
+
+        if self.verbose:
+            print "IrodsCommand:", " ".join([self.cmd] + self.opts + map(repr, cmdline))
+
+        p = subprocess.Popen( cmdlist, stdout = subprocess.PIPE, stderr = subprocess.STDOUT )
+        output = p.communicate()[0]
+
+        return p.returncode, self.output_filter(output)
+
+def parse_env(path):
+    "parse iRODS iCommands environment files"
+
+    envre = re.compile("^\s*(?P<name>\w+)\s*[=\s]\s*['\"]?(?P<value>[^'\"]*[^'\"\s])['\"]?\s*")
+
+    ret = {}
+
+    #print "opening", path
+    with open(path, 'r') as f:
+        for l in f.readlines():
+            #print "parsing", l
+            m = envre.match(l)
+            if m:
+                ret[m.group("name")] = m.group("value")
+    return ret
+
+def guess_icwd():
+    "guess iCommand working directory"
+    pid = os.getpid()
+    ppid = os.getppid()
+
+    repo = os.path.expanduser(os.path.join("~", ".irods"))
+
+    icwd = None
+    try:
+        icwd = parse_env(os.path.join(repo, ".irodsEnv.%d" % pid))["irodsCwd"]
+    except:
+        try:
+            icwd = parse_env(os.path.join(repo, ".irodsEnv.%d" % ppid))["irodsCwd"]
+        except:
+            pass
+
+    if not icwd:
+        ipwd = IrodsCommand("ipwd")
+        retcode, icwd = ipwd()
+
+    return icwd
+
+def guess_user():
+    "guess iCommands iRODS user name" 
+    envfile = os.path.expanduser(os.path.join("~", ".irods", ".irodsEnv"))
+
+    env = parse_env(envfile)
+
+    try:
+        user = env["irodsUserName"]
+    except:
+        user = os.getlogin()
+    return user
+
+def isrel(path):
+    "check if path is relative"
+    return not path.startswith('/')
+
