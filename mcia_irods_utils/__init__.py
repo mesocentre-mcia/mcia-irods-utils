58e86a693d23178b5ee5ccd5b08f09eda1217cfb
Pierre Gay <pierre.gay@u-bordeaux.fr>
2014-12-08 16:53:17 +0100

    merge irepl-check with mcia_irods_utils package

--- mcia_irods_utils/__init__.py
+++ mcia_irods_utils/__init__.py
@@ -1,3 +1,4 @@
 from icommand import IrodsCommand, guess_icwd, guess_user, guess_zone, guess_home, expanduser, isrel
 
 from wildcard import iswild, ipathw, iargw
+from replicas import iquest_replicas, mciaRGList, file_replicas
