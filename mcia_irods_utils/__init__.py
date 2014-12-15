1345b0e52049eaf858a4b074f11dd3754accc6be
Pierre Gay <pierre.gay@u-bordeaux.fr>
2014-12-15 15:49:37 +0100

    allow configuration of repair behaviour at .irodsEnv or shell environment level

--- mcia_irods_utils/__init__.py
+++ mcia_irods_utils/__init__.py
@@ -1,4 +1,4 @@
-from icommand import IrodsCommand, guess_icwd, guess_user, guess_zone, guess_home, expanduser, isrel, parse_env
+from icommand import IrodsCommand, guess_icwd, guess_user, guess_zone, guess_home, expanduser, isrel, parse_env, getenv
 
 from wildcard import iswild, ipathw, iargw
 from replicas import iquest_replicas, file_replicas
