0884387f9107625299fecac2148e1e4e1ba68375
Pierre Gay <pierre.gay@u-bordeaux.fr>
2015-03-05 17:01:10 +0100

    irepl-check: warn user when collection doesn't exist

--- mcia_irods_utils/__init__.py
+++ mcia_irods_utils/__init__.py
@@ -3,3 +3,4 @@ from icommand import IrodsCommand, guess_icwd, guess_user, guess_zone, guess_hom
 from wildcard import iswild, ipathw, iargw
 from replicas import iquest_replicas, file_replicas
 from metadata import iquest_collection_metadata
+from collection import iquest_iscollection
