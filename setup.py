5cef2e2f12b045156eddd50207ee63726ba13c80
Pierre Gay <peyo@libvsg.org>
2015-11-19 13:20:18 +0100

    bump version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 scripts = glob.glob( "scripts/i*" )
 
 setup( name = 'mcia-irods-utils',
-       version = '0.5.6',
+       version = '0.5.7',
        description = 'MCIA\'s iCommands utilities',
        author = 'Pierre Gay',
        author_email = 'pierre.gay@u-bordeaux.fr',
