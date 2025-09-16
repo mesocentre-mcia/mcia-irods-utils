8b3aac430f7746f14684442b799630a70d73f564
Pierre Gay <pierre.gay@u-bordeaux.fr>
2025-09-16 17:02:28 +0200

    another stupid fix

--- mcia_irods_utils/__init__.py
+++ mcia_irods_utils/__init__.py
@@ -1,5 +1,7 @@
-from .icommand import DirectOutputIrodsCommand, IrodsCommand, guess_icwd, guess_user, guess_zone, guess_home, expanduser, isrel, load_env, getenv
-
+from .icommand import (
+    DirectOutputIrodsCommand, IquestCommand, IrodsCommand, guess_icwd,
+    guess_user, guess_zone, guess_home, expanduser, isrel, load_env, getenv
+)
 from .wildcard import iswild, ipathw, iargw
 from .replicas import iquest_replicas, file_replicas, dataid_replicas
 from .metadata import iquest_collection_metadata
